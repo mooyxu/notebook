443ad2cfb3045fb1b637659b930ad3d96db07d7f
Marvin <356147564@qq.com>
2020-03-02 15:21:02 +0800

    Update run.sh
    added sleep

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 
 if [ ! -n "$1" ]; then
     /opt/anaconda/bin/jupyter notebook 2>&1 >/opt/notebook.log &
+    sleep 3
     /opt/anaconda/bin/jupyter lab
 else
     /opt/anaconda/bin/jupyter notebook --NotebookApp.password=$1 2>&1 >/opt/notebook.log &
+    sleep 3
     /opt/anaconda/bin/jupyter lab --NotebookApp.password=$1
 fi
